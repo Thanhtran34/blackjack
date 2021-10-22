b10c62bcd390e298611a1721a5023957f0cae1dd
Thanh Tran <tt222kj@student.lnu.se>
2021-10-22 19:45:34 +0200

    fix first requirement

--- src/main/java/model/Dealer.java
+++ src/main/java/model/Dealer.java
@@ -4,9 +4,7 @@ import model.rules.HitStrategy;
 import model.rules.NewGameStrategy;
 import model.rules.RulesFactory;
 
-/**
- * Represents a dealer player that handles the deck of cards and runs the game using rules.
- */
+/** Represents a dealer player that handles the deck of cards and runs the game using rules. */
 public class Dealer extends Player {
 
   private Deck deck;
@@ -15,7 +13,7 @@ public class Dealer extends Player {
 
   /**
    * Initializing constructor.
-
+   *
    * @param rulesFactory A factory that creates the rules to use.
    */
   public Dealer(RulesFactory rulesFactory) {
@@ -26,7 +24,7 @@ public class Dealer extends Player {
 
   /**
    * Starts a new game if the game is not currently under way.
-
+   *
    * @param player The player to play agains.
    * @return True if the game could be started.
    */
@@ -42,7 +40,7 @@ public class Dealer extends Player {
 
   /**
    * Gives the player one more card if possible. I.e. the player hits.
-
+   *
    * @param player The player to give a card to.
    * @return true if the player could get a new card, false otherwise.
    */
@@ -60,7 +58,7 @@ public class Dealer extends Player {
 
   /**
    * Checks if the dealer is the winner compared to a player.
-
+   *
    * @param player The player to check agains.
    * @return True if the dealer is the winner, false if the player is the winner.
    */
@@ -75,7 +73,7 @@ public class Dealer extends Player {
 
   /**
    * Checks if the game is over, i.e. the dealer can take no more cards.
-
+   *
    * @return True if the game is over.
    */
   public boolean isGameOver() {
@@ -85,12 +83,14 @@ public class Dealer extends Player {
     return false;
   }
 
-  /**
-   * The player has choosen to take no more cards, it is the dealers turn.
-   */
-  public boolean stand() {
-    //TODO: implement me
+  /** The player has choosen to take no more cards, it is the dealers turn. */
+  public boolean stand(Player player) {
+    if (deck != null) {
+      showHand();
+      while (hitRule.doHit(this) == true) {
+        hit(this);
+      }
+    }
     return false;
   }
-
 }

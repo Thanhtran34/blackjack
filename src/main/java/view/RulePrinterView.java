1fdba9ed069a8116674f3a55789c4c6bf0c8fe2b
Thanh Tran <tt222kj@student.lnu.se>
2021-11-04 14:27:09 +0100

    fix rule view by visitor

---
+++ src/main/java/view/RulePrinterView.java
@@ -0,0 +1,38 @@
+package view;
+
+import model.rules.GameVisitor;
+import model.rules.HitStrategy;
+import model.rules.NewGameStrategy;
+import model.rules.WinStrategy;
+
+public class RulePrinterView implements GameVisitor {
+    @Override
+    public void applyAmericanNewGameStrategy(NewGameStrategy newGameStrategy) {
+    System.out.println("New Game Strategy: American New Game Strategy!");
+    }
+
+    @Override
+    public void applyInternationalNewGameStrategy(NewGameStrategy newGameStrategy) {
+    System.out.println("New Game Strategy: International New Game Strategy!");
+    }
+
+    @Override
+    public void applyBasicHitStrategy(HitStrategy hitStrategy) {
+    System.out.println("Hit Strategy: Basic Hit Strategy!");
+    }
+
+    @Override
+    public void applySoftSevenTeenHitStrategy(HitStrategy hitStrategy) {
+    System.out.println("Hit Strategy: Soft17 Hit Strategy!");
+    }
+
+    @Override
+    public void applyDealerWinStrategy(WinStrategy dealerWinStrategy) {
+    System.out.println("Win Strategy: Dealer Win Strategy!");
+    }
+
+    @Override
+    public void applyPlayerWinStrategy(WinStrategy playerWinStrategy) {
+    System.out.println("Win Strategy: Player Win Strategy!");
+    }
+}
